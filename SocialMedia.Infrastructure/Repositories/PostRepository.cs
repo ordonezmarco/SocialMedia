ece7543f055f902c56d087f4d78636110d233c90
Marco Ordoñez <ordonez.marcos21@gmail.com>
2020-06-09 23:38:19 -0500

    crear dummy de datos de api controller

---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,26 @@
+﻿using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+
+        public IEnumerable<Post> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new Post
+            { 
+                PostId = x,
+                Description = $"Description {x}",
+                Image = $"https://misapis.com/{x}",
+                UserId = x * 2
+            });
+
+            return posts;
+        }
+    }
+}
